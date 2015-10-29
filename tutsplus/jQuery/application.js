0d07a1528bd249e03e4cff45328525d641f764ea
Jake R. E. Hill <jake_hill666@hotmail.co.uk>
2015-10-29 11:38:05 +0100

    make button to switch css with jQuery

---
+++ tutsplus/jQuery/application.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  var link = $("link.custom");
+  $("button").click(function() {
+    var $this = $(this),
+        stylesheet = $this.data("file");
+    
+    link.attr("href", stylesheet + ".css");
+    
+    $this
+        .siblings("button")
+            .removeAttr("disabled")
+            .end()
+        .attr("disabled", "disabled");
+  });
+});
