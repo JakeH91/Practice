e5d3f5ea0377898d32277feb7917733f8bead9cd
Jake R. E. Hill <jake_hill666@hotmail.co.uk>
2015-11-27 11:50:16 +0100

    add animation to complete homework

--- tutsplus/jQuery/javascript/homework-3-5.js
+++ tutsplus/jQuery/javascript/homework-3-5.js
@@ -1,9 +1,12 @@
 $(document).ready(function(){
   var target = $('div.target');
   target.hide();
-  // $('button').on('click', function(){
-  //     target.animate({
-
-  //     })
-  //   });)
+  $('button').on('click', function(){
+      target.animate({
+            'height': 'toggle',
+            'width': 'toggle',
+            'opacity': 'toggle',
+            'border-radius': 'toggle'
+          }, 500);
+    });
 });
