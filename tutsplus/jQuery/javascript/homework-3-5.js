18b22ca357713f232d9634914538c0b95b3e0981
Jake R. E. Hill <jake_hill666@hotmail.co.uk>
2015-11-27 12:02:55 +0100

    create custom function, add return

--- tutsplus/jQuery/javascript/homework-3-5.js
+++ tutsplus/jQuery/javascript/homework-3-5.js
@@ -1,12 +1,17 @@
 $(document).ready(function(){
   var target = $('div.target');
   target.hide();
-  $('button').on('click', function(){
-      target.animate({
+
+  $.fn.FadeSlideToggle = function(){
+    return $(this).animate({
             'height': 'toggle',
             'width': 'toggle',
             'opacity': 'toggle',
             'border-radius': 'toggle'
           }, 500);
+  };
+
+  $('button').on('click', function(){
+      target.FadeSlideToggle();
     });
 });
