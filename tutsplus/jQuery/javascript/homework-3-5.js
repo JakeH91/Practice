ab3b4a9b4f6d46f3f62a23d20a416288e5476f6c
Jake R. E. Hill <jake_hill666@hotmail.co.uk>
2015-11-27 11:35:56 +0100

    hide target

--- tutsplus/jQuery/javascript/homework-3-5.js
+++ tutsplus/jQuery/javascript/homework-3-5.js
@@ -1,3 +1,9 @@
 $(document).ready(function(){
-  
+  var target = $('div.target');
+  target.hide();
+  // $('button').on('click', function(){
+  //     target.animate({
+
+  //     })
+  //   });)
 });
