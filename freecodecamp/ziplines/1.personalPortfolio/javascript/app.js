5e6a1bf37c53e17ee4f91c5b39f794f1898e9fbd
Jake R. E. Hill <jake_hill666@hotmail.co.uk>
2016-03-15 13:52:55 +0100

    Change #section-about RESPONSIVE v2

--- freecodecamp/ziplines/1.personalPortfolio/javascript/app.js
+++ freecodecamp/ziplines/1.personalPortfolio/javascript/app.js
@@ -1,11 +1,11 @@
 $(window).load(function(){
   var height;
   var aboutWidth = $("#section-about").width();
-    /*if (aboutWidth >= 713){
+    if (aboutWidth >= 713){
       height = $(".about-text").outerHeight();
       $(".new").height(height);
     }
-    else */if (/*aboutWidth < 713 && */aboutWidth > 647){
+    else if (aboutWidth < 713 && aboutWidth > 647){
       height = $(".about-text").outerHeight() + $(".connect").outerHeight();
       $(".new").height(height);
     }
@@ -17,12 +17,12 @@ $(window).load(function(){
 
   $(window).resize(function(){
     var aboutWidth = $("#section-about").width();
-    /*if (aboutWidth >= 713){
+    if (aboutWidth >= 713){
       height = $(".about-text").outerHeight();
       $(".new").height(height);
       $("#section-about").css("padding-bottom", 40);
     }
-    else */if (/*aboutWidth < 713 && */aboutWidth > 647){
+    else if (aboutWidth < 713 && aboutWidth > 647){
       height = $(".about-text").outerHeight() + $(".connect").outerHeight();
       $(".new").height(height);
       $("#section-about").css("padding-bottom", 40);
