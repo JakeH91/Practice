4304f8f44d694814397ceb9a56f82f86fcb01bd6
Jake R. E. Hill <jake_hill666@hotmail.co.uk>
2016-03-15 13:50:51 +0100

    Change #section-about RESPONSIVE v1

---
+++ freecodecamp/ziplines/1.personalPortfolio/javascript/app.js
@@ -0,0 +1,37 @@
+$(window).load(function(){
+  var height;
+  var aboutWidth = $("#section-about").width();
+    /*if (aboutWidth >= 713){
+      height = $(".about-text").outerHeight();
+      $(".new").height(height);
+    }
+    else */if (/*aboutWidth < 713 && */aboutWidth > 647){
+      height = $(".about-text").outerHeight() + $(".connect").outerHeight();
+      $(".new").height(height);
+    }
+    else if (aboutWidth <= 647){
+      height = $(".about-text").outerHeight();
+      $(".new").height(height);
+      $("#section-about").css("padding-bottom", height);
+    }
+
+  $(window).resize(function(){
+    var aboutWidth = $("#section-about").width();
+    /*if (aboutWidth >= 713){
+      height = $(".about-text").outerHeight();
+      $(".new").height(height);
+      $("#section-about").css("padding-bottom", 40);
+    }
+    else */if (/*aboutWidth < 713 && */aboutWidth > 647){
+      height = $(".about-text").outerHeight() + $(".connect").outerHeight();
+      $(".new").height(height);
+      $("#section-about").css("padding-bottom", 40);
+    }
+    else if (aboutWidth <= 647){
+      height = $(".about-text").outerHeight();
+      $(".new").height(height);
+      $("#section-about").css("padding-bottom", height);
+    }
+    
+  });
+});
